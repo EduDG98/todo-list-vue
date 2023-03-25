67b0ad2ae8f6f31ce67e7912e5096524b10efb18
Eduardo Diaz <edudug98@gmail.com>
2023-03-25 20:26:54 +0000

    create project and structure

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <label for="input-wrapper">
+      <input type="text" id="input-wrapper">
+      <button> Add </button>
+    </label>
+    <ul>
+      <li>
+        <span>To Jump</span>
+        <button>✖</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
